d404dc7fada82e8cc6ac8a0a312604ac5f2bba58
dmitrijbuchnev <buchnev2003.03@mail.ru>
2024-01-26 20:40:21 +0300

    Init twitter app

---
+++ src/test/java/dev/simpleapp/twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simpleapp.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+	//jdbc:postgresql://${TWITTER_DB_HOST}:${TWITTER_DB_PORT}/${TWITTER_DB_NAME}
+	@Test
+	void contextLoads() {
+	}
+
+}
