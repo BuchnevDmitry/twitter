9aaadd3a1b3154764a60a395d6dff2654abe01d1
dmitrijbuchnev <buchnev2003.03@mail.ru>
2024-01-30 03:22:43 +0300

    UserProfile first implementation

---
+++ src/main/java/dev/simpleapp/twitter/security/api/service/impl/IdentityApiServiceImpl.java
@@ -0,0 +1,36 @@
+package dev.simpleapp.twitter.security.api.service.impl;
+
+import dev.simpleapp.twitter.security.api.model.CurrentUserApiModel;
+import dev.simpleapp.twitter.security.api.service.IdentityApiService;
+import dev.simpleapp.twitter.security.service.UserAccountService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class IdentityApiServiceImpl implements IdentityApiService {
+
+    private final UserAccountService userAccountService;
+
+    public IdentityApiServiceImpl(UserAccountService userAccountService) {
+        this.userAccountService = userAccountService;
+    }
+
+    @Override
+    public Optional<CurrentUserApiModel> currentUserAccount() {
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication()) 
+                .map(Authentication::getName)
+                .flatMap(this::extractCurrentUserApiModel);
+
+    }
+
+    private Optional<CurrentUserApiModel> extractCurrentUserApiModel(String username) {
+        return userAccountService.findUserByUsername(username)
+                .map(userAccount -> new CurrentUserApiModel(userAccount.getId()));
+    }
+}
