cb3c50c702b6945c9a703ddef843f2eca817db69
BuchnevDmitry <buchnev03.03@mail.ru>
2024-02-05 14:35:56 +0300

    Find tweets first implementation

--- src/main/java/dev/simpleapp/twitter/user/tweet/service/TweetService.java
+++ src/main/java/dev/simpleapp/twitter/user/tweet/service/TweetService.java
@@ -1,13 +1,15 @@
 package dev.simpleapp.twitter.user.tweet.service;
 
+import dev.simpleapp.twitter.user.profile.model.UserProfile;
 import dev.simpleapp.twitter.user.tweet.model.Tweet;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface TweetService {
     Tweet createTweet(Tweet tweet);
     Tweet updateTweet(Tweet tweet);
     Optional<Tweet> findTweetById(long tweetId);
-
     void deleteTweet(long tweetId);
+    Collection<Tweet> findAllTweets(UserProfile owner);
 }
